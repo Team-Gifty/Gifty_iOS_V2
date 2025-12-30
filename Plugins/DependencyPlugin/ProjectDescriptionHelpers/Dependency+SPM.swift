37c2b5892ee2784a6560d8de927ef93b2410f17b
Juhui <joohee090331@gmail.com>
2025-12-30 17:00:39 +0900

    chore :: Dependency+SPM 내부 구현

---
+++ Plugins/DependencyPlugin/ProjectDescriptionHelpers/Dependency+SPM.swift
@@ -0,0 +1,22 @@
+import ProjectDescription
+
+public extension TargetDependency.PackageType {
+    static let Moya = TargetDependency.external(name: "Moya")
+    static let RxSwift = TargetDependency.external(name: "RxSwift")
+    static let SnapKit = TargetDependency.external(name: "SnapKit")
+    static let Then = TargetDependency.external(name: "Then")
+    static let RxCocoa = TargetDependency.external(name: "RxCocoa")
+    static let RxMoya = TargetDependency.external(name: "RxMoya")
+    static let RxFlow = TargetDependency.external(name: "RxFlow")
+    static let Kingfisher = TargetDependency.external(name: "Kingfisher")
+    static let RxGesture = TargetDependency.external(name: "RxGesture")
+    static let Swinject = TargetDependency.external(name: "Swinject")
+    static let Lottie = TargetDependency.external(name: "Lottie")
+    static let SkeletonView = TargetDependency.external(name: "SkeletonView")
+    static let FSCalendar = TargetDependency.external(name: "FSCalendar")
+    static let RxDataSources = TargetDependency.external(name: "RxDataSources")
+    static let ReactorKit = TargetDependency.external(name: "ReactorKit")
+    static let Starscream = TargetDependency.external(name: "Starscream")
+    static let Siren = TargetDependency.external(name: "Siren")
+}
+
