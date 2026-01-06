ef26c0fa110373131b5b126fc50e8733d05e9d01
Juhui <joohee090331@gmail.com>
2026-01-06 22:51:23 +0900

    chore :: Package 파일 수정

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -1,27 +1,3 @@
-// swift-tools-version: 6.0
-import PackageDescription
-
-#if TUIST
-    import struct ProjectDescription.PackageSettings
-
-    let packageSettings = PackageSettings(
-        // Customize the product types for specific package product
-        // Default is .staticFramework
-        // productTypes: ["Alamofire": .framework,]
-        productTypes: [:]
-    )
-#endif
-
-let package = Package(
-    name: "Gifty_V2",
-    dependencies: [
-        // Add your own dependencies here:
-        // .package(url: "https://github.com/Alamofire/Alamofire", from: "5.0.0"),
-        // You can read more about dependencies here: https://docs.tuist.io/documentation/tuist/dependencies
-    ]
-)
-
-
 // swift-tools-version: 6.1
 
 import PackageDescription
@@ -67,7 +43,7 @@ let package = Package(
     ],
     targets: [
         .target(
-            name: "GiftyKPackage",
+            name: "GiftyPackage",
             dependencies: [
                 "RxSwift",
                 .product(name: "RxCocoa", package: "RxSwift"),
@@ -86,3 +62,4 @@ let package = Package(
         )
     ]
 )
+
